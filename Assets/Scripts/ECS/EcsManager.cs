5a41d0f302ee6860e2be0331da4d7aea3c382592
Francisco Cuauhtemoc Castañeda Rovira <31245060+dragaus@users.noreply.github.com>
2022-05-05 09:07:46 -0500

    Add ecs manager

--- Assets/Scripts/ECS/EcsManager.cs
+++ Assets/Scripts/ECS/EcsManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public class EcsManager : MonoBehaviour
 {
+    EntityManager manager;
+    public GameObject zombunnyPrefab;
+    const int numOfZombunnies = 2000;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings
+            .FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var entityPrefab = GameObjectConversionUtility
+            .ConvertGameObjectHierarchy(zombunnyPrefab, settings);
+        for (int i = 0; i < numOfZombunnies; i++)
+        {
+            var instance = manager.Instantiate(entityPrefab);
+            var position = transform.TransformPoint(
+                new float3(
+                    UnityEngine.Random.Range(-40, 40), 0, UnityEngine.Random.Range(-40, 40))
+                );
+            manager.SetComponentData(instance, new Translation { Value = position });
+            manager.SetComponentData(instance, new Rotation { Value = new quaternion(0, 0, 0, 0) });
+        }
     }
 }
